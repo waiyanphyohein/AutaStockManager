b2277d9fa23b1c71f4d5f03f5b57a39a100f5831
waiyanphyohein <waiyanphyohein@gmail.com>
2017-12-29 18:55:54 -0500

    Reuploading the files

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
+router.use('/chart',require('./chart'));
 router.use('/login', require('./login'));
 router.use('/signup', require('./signup'));
 router.use('/profile', require('./profile'));
 router.use('/', require('./home'));
+router.use('/table',require('./table'));
+router.use('/form',require('./form'));
 router.use('/logout',require('./logout'));
 module.exports = router;
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,29 @@
   const router = require('express').Router();
   const Redirect = require('../middlewares/redirect');
-
+  var mysql      = require('mysql');
+  var rows;
+  router.get('/', (req, res) => {
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'waiyan123',
+    database : 'auth-demo'
+  });
+  connection.connect();
+  connection.query('SELECT * from transactions', function(err, rows, fields) {
+    res.render('./profile/index.handlebars',{ Nutshell : rows});
+    if (!err)
+    {
+      console.log('The solution is: ', rows);
+    }
+    
+    else
+      console.log('Error while performing Query.');
+  });
+  
+    connection.end();
+  });
+  
 
   router.get('/',Redirect.ifNotLoggedIn(), (req, res) => {
     res.render('profile', { user: req.user });
